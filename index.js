bc9291dd971744106659b1504b75ad706a030c3c
Paramita Nandi <88779178+CuriousPNCoder@users.noreply.github.com>
2024-04-24 17:59:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-import jsonServer from "json-server";
-
 const jsonServer = require("json-server")
 const server = jsonServer.create();
 const router = jsonServer.router("categories.json")
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3032;
 
 server.use(middlewares);
 server.use(router);
